63afa2ac7afc5d384e6b321e20b9faa0410218ef
drdlee <danielrudi.lee@gmail.com>
2018-02-27 20:51:16 +0800

    adding firebase to project

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { Header } from './src/component/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDjeA01PkmD6VYPpoqDoP3N6WzTuM-zh4k',
+      authDomain: 'rn-auth-c8628.firebaseapp.com',
+      databaseURL: 'https://rn-auth-c8628.firebaseio.com',
+      projectId: 'rn-auth-c8628',
+      storageBucket: 'rn-auth-c8628.appspot.com',
+      messagingSenderId: '507444150307'
+    });
+  }
   render() {
     return (
       <Header headerText="What app?" />
